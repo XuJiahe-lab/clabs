2801b10a01fceddbffbaa72387cc8a32bf3868bd
XuJiahe-lab <56863155+XuJiahe-lab@users.noreply.github.com>
2019-11-12 13:38:35 +0800

    Update Ex3.c

--- lab3/Ex3.c
+++ lab3/Ex3.c
@@ -28,56 +28,65 @@ int main(void)
     {
         i++;
     }
-    for (i = 0; Z[i] != '\n'; i++)
+    if (Z[0] == '0' && (Z[1] == 'x' || Z[1] == 'X'))
     {
-        for (k = 0; Z[i] != Y[k] && k < 24; k++)
-            ;
-        if (k > 23)
+        for (i = 0; Z[i] != '\n'; i++)
         {
-            printf("输入了不合法的字符\n");
-            return -1;
+            for (k = 0; Z[i] != Y[k] && k < 24; k++)
+                ;
+            if (k > 23)
+            {
+                printf("输入了不合法的字符\n");
+                return -1;
+            }
+            if (i > 1)
+            {
+                if (Z[i] >= '0' && Z[i] <= '9')
+                {
+                    n = n * 16 + (Z[i] - '0');
+                }
+                else
+                {
+                    if (Z[i] == 'a' || Z[i] == 'A')
+                    {
+                        p = 10;
+                    }
+                    else if (Z[i] == 'b' || Z[i] == 'B')
+                    {
+                        p = 11;
+                    }
+                    else if (Z[i] == 'c' || Z[i] == 'C')
+                    {
+                        p = 12;
+                    }
+                    else if (Z[i] == 'd' || Z[i] == 'D')
+                    {
+                        p = 13;
+                    }
+                    else if (Z[i] == 'e' || Z[i] == 'E')
+                    {
+                        p = 14;
+                    }
+                    else if (Z[i] == 'f' || Z[i] == 'F')
+                    {
+                        p = 15;
+                    }
+                    n = n * 16 + p;
+                }
+            }
         }
-        if (i > 1)
+
+        if (i <= 10)
         {
-            if (Z[i] >= '0' && Z[i] <= '9')
-            {
-                n = n * 16 + (Z[i] - '0');
-            }
-            else
-            {
-                if (Z[i] == 'a' || Z[i] == 'A')
-                {
-                    p = 10;
-                }
-                else if (Z[i] == 'b' || Z[i] ==  'B')
-                {
-                    p = 11;
-                }
-                else if (Z[i] == 'c' || Z[i] ==  'C')
-                {
-                    p = 12;
-                }
-                else if (Z[i] == 'd' || Z[i] ==  'D')
-                {
-                    p = 13;
-                }
-                else if (Z[i] == 'e' || Z[i] ==  'E')
-                {
-                    p = 14;
-                }
-                else if (Z[i] == 'f' || Z[i] ==  'F')
-                {
-                    p = 15;
-                }
-                n = n * 16 + p;
-            }
+            printf("输入数字的十进制表达: %u\n", n);
+        }
+        else
+        {
+            printf("输入的数字太大，需要换一个小的数字\n");
+            printf("系统最大能够接受整数：%u\n", h - 1);
         }
     }
-    if (i <= 10){
-    printf("输入数字的十进制表达: %u\n", n);
-    }
-    else{
-        printf("输入的数字太大，需要换一个小的数字\n");
-        printf("系统最大能够接受整数：%u\n",h - 1);
+    else {
+    printf("输入了不合法的字符\n");
     }
 }
